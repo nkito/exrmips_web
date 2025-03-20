dc6c35835e73d7bc8090f56a8f82f273fb2a1198
Nobutaka Kito <nkito@sist.chukyo-u.ac.jp>
2025-03-21 00:59:33 +0900

    Update WASM

--- pkg/exrmips1.js
+++ pkg/exrmips1.js
@@ -379,7 +379,7 @@ function __wbg_get_imports() {
         const ret = false;
         return ret;
     };
-    imports.wbg.__wbindgen_closure_wrapper219 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper220 = function(arg0, arg1, arg2) {
         const ret = makeMutClosure(arg0, arg1, 43, __wbg_adapter_20);
         return ret;
     };
